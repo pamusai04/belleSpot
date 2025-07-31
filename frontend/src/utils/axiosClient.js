7b6ac79092f9a3f0b6e28f5751a54fcac4a6cb35
pamusai <pamusai04102003@gmail.com>
2025-07-31 17:33:35 +0530

    production update9

--- frontend/src/utils/axiosClient.js
+++ frontend/src/utils/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-  baseURL: 'https://bellespot.onrender.com',
+  baseURL: 'https://bellespots.onrender.com',
   // baseURL : 'http://localhost:3000',
   withCredentials: true, 
   timeout: 10000,
