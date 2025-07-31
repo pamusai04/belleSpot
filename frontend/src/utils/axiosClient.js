22e02255441dcd34e0daf7296ab55bb35ed8b3f3
pamusai <pamusai04102003@gmail.com>
2025-07-31 14:25:51 +0530

    production update6

--- frontend/src/utils/axiosClient.js
+++ frontend/src/utils/axiosClient.js
@@ -2,16 +2,23 @@ import axios from 'axios';
 
 const axiosClient = axios.create({
   baseURL: process.env.NODE_ENV === 'production' ? 'https://bellespot.onrender.com' : 'http://localhost:3000',
+  
   withCredentials: true, 
   timeout: 10000,
 });
 
 axiosClient.interceptors.request.use(
   (config) => {
-    
+    console.log('Axios request:', {
+      url: config.url,
+      method: config.method,
+      headers: config.headers,
+      cookies: document.cookie, 
+    });
     return config;
   },
   (error) => {
+    
     return Promise.reject(error);
   }
 );
