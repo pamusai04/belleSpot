1661001aa356a52863fbab161d03a90cbcf14fa9
pamusai <pamusai04102003@gmail.com>
2025-09-18 20:10:58 +0530

    Initial project commit9

--- frontend/src/utils/axiosClient.js
+++ frontend/src/utils/axiosClient.js
@@ -2,20 +2,26 @@ import axios from 'axios';
 
 const axiosClient = axios.create({
   baseURL: 'https://bellespots.onrender.com',
-  // baseURL: 'https://bellespot.onrender.com',
-  // baseURL: 'http://localhost:3000',
+  // baseURL : 'https://bellespot.onrender.com',
+  // baseURL:'http://localhost:3000',
   withCredentials: true, 
-  timeout: 0, 
+  timeout: 10000,
 });
 
 axiosClient.interceptors.request.use(
-  (config) => config,
-  (error) => Promise.reject(error)
+  (config) => {
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
 );
 
 axiosClient.interceptors.response.use(
   (response) => response,
-  (error) => Promise.reject(error)
+  (error) => {
+    return Promise.reject(error);
+  }
 );
 
 export default axiosClient;
